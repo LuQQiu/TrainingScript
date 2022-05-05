a53bb3edd3ffc26f7589dbb989d0ac6a63990824
Lu Qiu <luqiujob@gmail.com>
2022-05-05 11:44:15 -0700

    Fix init env

--- load.py
+++ load.py
@@ -62,8 +62,7 @@ class LocalDataset(Dataset):
 
 
 def start_load(args):
-    torch.distributed.init_process_group(backend='gloo',
-                                         init_method='env://')
+    torch.distributed.init_process_group(backend='gloo')
     dataset = LocalDataset(size=args.number_of_files, filelist=args.file_name_list, prefix=args.path_prefix)
     sampler = DistributedSampler(dataset, shuffle=True)
     data_loader = torch.utils.data.DataLoader(
