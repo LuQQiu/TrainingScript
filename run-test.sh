f286f7c272a1307f30ace7498979b0d4a47c7666
Lu Qiu <luqiujob@gmail.com>
2022-05-05 16:10:48 -0700

    add time to wait

--- run-test.sh
+++ run-test.sh
@@ -8,4 +8,7 @@ rm -rf $PRO_TMP_DIR
 mkdir -p $PRO_TMP_DIR
 export PROMETHEUS_MULTIPROC_DIR=$PRO_TMP_DIR
 
+if [[ ${MASTER_ADDR} != "localhost" ]];
+   sleep(100000)
+fi
 python -m torch.distributed.launch --master_addr="${MASTER_ADDR}" --master_port=${MASTER_PORT} --nnodes=${WORLD_SIZE} --node_rank=${RANK} --nproc_per_node=${nproc_per_node} $@
