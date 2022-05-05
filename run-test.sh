7f5b54bd527caaebe83686a407c1dfa3aaff7079
Lu Qiu <luqiujob@gmail.com>
2022-05-05 14:31:10 -0700

    Remove master address

--- run-test.sh
+++ run-test.sh
@@ -8,4 +8,4 @@ rm -rf $PRO_TMP_DIR
 mkdir -p $PRO_TMP_DIR
 export PROMETHEUS_MULTIPROC_DIR=$PRO_TMP_DIR
 
-python -m torch.distributed.launch --master_addr="${MASTER_ADDR}" --master_port=${MASTER_PORT} --nnodes=${WORLD_SIZE} --node_rank=${RANK} --nproc_per_node=${nproc_per_node} $@
+python -m torch.distributed.launch --nnodes=${WORLD_SIZE} --node_rank=${RANK} --nproc_per_node=${nproc_per_node} $@
