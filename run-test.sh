abe5793956ce59a1500cc7dfc21b260d385333a0
Lu Qiu <luqiujob@gmail.com>
2022-05-04 17:11:56 -0700

    fix

--- run-test.sh
+++ run-test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+nproc_per_node=${1}
+shift
+
 PRO_TMP_DIR=/tmp/pro_metrics/
 rm -rf $PRO_TMP_DIR
 mkdir -p $PRO_TMP_DIR
@@ -8,6 +11,4 @@ export PROMETHEUS_MULTIPROC_DIR=$PRO_TMP_DIR
 nnode=${WORLD_SIZE:-1}
 node_rank=${RANK:-0}
 
-nproc_per_node=${1}
-shift
-python -m torch.distributed.run --nnode ${nnode} --node_rank ${RANK} --nproc_per_node $num_proc $@
+python -m torch.distributed.run --nnode ${nnode} --node_rank ${RANK} --nproc_per_node ${nproc_per_node} $@
