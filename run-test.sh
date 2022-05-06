2cb84ba1fffaa7a5d43f39f19d92152c777c837d
Lu Qiu <luqiujob@gmail.com>
2022-05-06 15:29:51 -0700

    Fix

--- run-test.sh
+++ run-test.sh
@@ -8,5 +8,10 @@ rm -rf $PRO_TMP_DIR
 mkdir -p $PRO_TMP_DIR
 export PROMETHEUS_MULTIPROC_DIR=$PRO_TMP_DIR
 
-echo "python -m torch.distributed.launch --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT} --nnodes=${WORLD_SIZE} --node_rank=${RANK} --nproc_per_node=${nproc_per_node}"
+WORLD_SIZE=${WORLD_SIZE:-1}
+if [[ ${WORLD_SIZE} -eq 1 ]]; then
+  python -m torch.distributed.run --nproc_per_node $num_proc $@
+  return $?
+fi
+
 python -m torch.distributed.launch --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT} --nnodes=${WORLD_SIZE} --node_rank=${RANK} --nproc_per_node=${nproc_per_node} $@
